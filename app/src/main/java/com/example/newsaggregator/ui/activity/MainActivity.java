e4dfddcfc8ac934f70aca57c1aac1f66d6305eef
Nthcrjk <arfilippov@bk.ru>
2021-01-13 05:47:22 +0300

    Добавил ToolBar и кнопку назад для него

--- app/src/main/java/com/example/newsaggregator/ui/activity/MainActivity.java
+++ app/src/main/java/com/example/newsaggregator/ui/activity/MainActivity.java
@@ -5,31 +5,40 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.appcompat.widget.Toolbar;
 
 import com.example.newsaggregator.R;
-import com.example.newsaggregator.model.model.NewsModel;
-import com.example.newsaggregator.model.service.Service;
 import com.example.newsaggregator.ui.fragments.OnNavigationListener;
 import com.example.newsaggregator.ui.fragments.flowFragments.MainFlowFragment;
-
-import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
-import io.reactivex.rxjava3.annotations.NonNull;
-import io.reactivex.rxjava3.core.Observable;
-import io.reactivex.rxjava3.core.Observer;
-import io.reactivex.rxjava3.disposables.Disposable;
-import io.reactivex.rxjava3.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity implements OnNavigationListener {
 
     public final static String API_KEY = "4efaaafdb72d4925a544b61efe8bf931";
 
     private FragmentTransaction fTrance;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.tool_bar);
+
+        toolbar.setNavigationIcon(R.drawable.abc_vector_test);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+        toolbar.setTitle("");
+
         onNavigate(new MainFlowFragment(), false);
     }
 
